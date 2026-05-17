094afdb5c2661a785ef0b09895815dfd0d32e3fe
허승빈 <qmfhfl02@gmail.com>
2026-05-17 17:54:14 +0900

    revert lab session changes

--- sketch.js
+++ sketch.js
@@ -63,12 +63,6 @@ function draw() {
     if(button.show)
       button.display();
   }
-
-  // 각자 자기 성에 맞는 파일 생성 후 푸시하기
-  // kim.js
-  // kwon.js
-  // lee.js
-  // kimJ.js
 }
 
 function keyPressed(){
